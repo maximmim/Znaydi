bc5e8056d9b8af6e6a695326cd9c6d73df760bda
maximmim <93773088+maximmim@users.noreply.github.com>
2025-01-28 22:14:49 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -23,38 +23,36 @@ app.use(cors());
 
 async function getAllData() {
     const client = new MongoClient(uri, {});
-  
+
     try {
-      await client.connect();
-  
-      const database = client.db(dbName);
-      const collection = database.collection(collectionName);
-  
-  
-      const result = await collection.find({}).toArray();
-  
-  
-      return result;
+        await client.connect();
+
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const result = await collection.find({}).toArray();
+
+        return result;
     } catch (error) {
-      console.error(error);
-      throw error; 
+        console.error(error);
+        throw error; 
     } finally {
-      await client.close();
+        await client.close();
     }
 }
 
 app.get('/get_db', async (req, res) => {
     try {
-      const data = await getAllData();
-      res.status(200).json(data);
+        const data = await getAllData();
+        res.status(200).json(data);
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: error });
+        console.error(error);
+        res.status(500).json({ message: error });
     }
-  });
+});
 
 app.post('/New', async (req, res) => {
-    console.log(123123)
+    console.log(123123);
     await bot.sendMessage(req.body.idtg, `Учень ${req.body.nick} зареєструвався`);
 });
 
@@ -62,58 +60,57 @@ app.put('/put_db/:id', async (req, res) => {
     const id = req.params.id;
     let updatedData = req.body;
     const client = new MongoClient(uri, {});
-  
+
     try {
-      await client.connect();
-      const database = client.db(dbName);
-      const collection = database.collection(collectionName);
-      if (updatedData._id) {
+        await client.connect();
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+        if (updatedData._id) {
             delete updatedData._id;
-      }
-  
-  
-      await collection.updateOne({ _id: new ObjectId(id) }, { $set: updatedData });
-  
+        }
+
+        await collection.updateOne({ _id: new ObjectId(id) }, { $set: updatedData });
+
+        res.status(200).json({ message: 'Запис успішно оновлений' });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: error});
+        console.error(error);
+        res.status(500).json({ message: error });
     } finally {
-      await client.close();
+        await client.close();
     }
 });
 
 app.post('/push_db', async (req, res) => {
+    const client = new MongoClient(uri, {});
+
     try {
-      const database = client.db(dbName);
-      const collection = database.collection(collectionName); 
-      const client = new MongoClient(uri, { });
-      await client.connect();
-  
-      const result = await collection.insertOne(req.body);
-      res.status(200).json({ message: 'Дані успішно збережено в MongoDB', insertedId: result.insertedId });
-      }
-      catch (error) {
-      console.error('Помилка при збереженні даних в MongoDB:', error);
-      res.status(500).json({ message: 'Виникла помилка при збереженні даних' });
-      }
-      finally {
-      client.close();
+        await client.connect();
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const result = await collection.insertOne(req.body);
+        res.status(200).json({ message: 'Дані успішно збережено в MongoDB', insertedId: result.insertedId });
+    } catch (error) {
+        console.error('Помилка при збереженні даних в MongoDB:', error);
+        res.status(500).json({ message: 'Виникла помилка при збереженні даних' });
+    } finally {
+        await client.close();
     }
 });
 
 
 const options = {
     reply_markup: {
-      keyboard: [
-        [
-          {
-            text: "Відкрити додаток",
-            web_app: { url: "https://dfc9-159-224-180-159.ngrok-free.app/" }  
-          }
-        ]
-      ],
-      resize_keyboard: true, 
-      one_time_keyboard: true  
+        keyboard: [
+            [
+                {
+                    text: "Відкрити додаток",
+                    web_app: { url: "https://dfc9-159-224-180-159.ngrok-free.app/" }  
+                }
+            ]
+        ],
+        resize_keyboard: true, 
+        one_time_keyboard: true  
     }
 };
 
