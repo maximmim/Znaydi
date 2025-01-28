e51029ac962e1e2d0c05420a62c406908aaf61bf
maximmim <93773088+maximmim@users.noreply.github.com>
2025-01-28 20:29:59 +0200

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,24 @@ import { useEffect,useState } from "react";
 
 
 const Home = () => {
-        const [userData, setUserData] = useState({});
-
+    const [userData, setUserData] = useState({});
+        
+    const [userID, setUserId] = useState(null);
+        
     useEffect(() => {
 
         try {
+        const tg = window.Telegram.WebApp;
           const telegramInitData = window.Telegram.WebApp.initDataUnsafe;
+
+          tg.setBackgroundColor("#ffffff");
+            
+
           const { first_name, last_name, username, photo_url } =
             telegramInitData.user;
+          
           setUserData({ first_name, last_name, username, photo_url });
-    
+            console.log(photo_url)
           if (telegramInitData?.user?.id) {
             setUserId(telegramInitData.user.id);
           }
@@ -21,7 +29,7 @@ const Home = () => {
     }, []);
     return ( 
         <>
-            <h1 className="fwaawg">{userData}</h1>
+            <h1 className="fwaawg">userData</h1>
         </>
      );
 }
